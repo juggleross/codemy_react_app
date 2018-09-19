7260410d1f6b1d1026dc84c631b1f1feab875889
Vladislav Hilko <vladislav.spawn@gmail.com>
2018-09-19 22:04:41 +0300

    Add react hello world

---
+++ src/HelloReact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HelloReact.css';
+
+class HelloReact extends React.Component {
+  render() {
+    return(
+      <div id='helloReact'>Hello </div>
+    )
+  }
+}
+
+export default HelloReact;
