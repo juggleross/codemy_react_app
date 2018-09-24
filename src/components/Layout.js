b1c41b95a08b6b9582c45d3af5ea974aa9b98dd7
Vladislav Hilko <vladislav.spawn@gmail.com>
2018-09-24 21:01:33 +0300

    Chapter 7 - props and nested components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './Layout.css';
 
+const data = [
+  { name: 'Vlad', email: 'vlad@tut.by'},
+  { name: 'Vlad2', email: 'vlad2@tut.by'},
+  { name: 'Vlad3', email: 'vlad3@tut.by'}
+];
+
+const Contact = (props) =>
+  <div className="pure-u-1-3">
+    <h1>{props.name}</h1>
+    <p>{props.email}</p>
+  </div>;
+
 class Layout extends React.Component {
   render() {
     return(
       <div id='helloReact' className='pure-g'>
-        <div className="pure-u-1-3"><p>Thirds</p></div>
-        <div className="pure-u-1-3"><p>Thirds</p></div>
-        <div className="pure-u-1-3"><p>Thirds</p></div>
+        { data.map(info =>
+          <Contact {...info}/>
+        )}
       </div>
     )
   }
