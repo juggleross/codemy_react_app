82d15cec11d24fd977a01e9f695566d5decb9979
Vladislav Hilko <vladislav.spawn@gmail.com>
2018-09-24 20:16:52 +0300

    Finished chapter 5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HelloReact from './HelloReact'
+import Layout from './components/Layout'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HelloReact />
+        <Layout />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Layout.css';
+
+class Layout extends React.Component {
+  render() {
+    return(
+      <div id='helloReact'> Hello </div>
+    )
+  }
+}
+
+export default Layout;
