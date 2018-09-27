dabf73bd77038d123387682e044111917051a57c
Vladislav Hilko <vladislav.spawn@gmail.com>
2018-09-27 22:22:01 +0300

    Chapter 8 - clean up our app

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import './Layout.css';
-
-const data = [
-  { name: 'Vlad', email: 'vlad@tut.by'},
-  { name: 'Vlad2', email: 'vlad2@tut.by'},
-  { name: 'Vlad3', email: 'vlad3@tut.by'}
-];
-
-const Contact = (props) =>
-  <div className="pure-u-1-3">
-    <h1>{props.name}</h1>
-    <p>{props.email}</p>
-  </div>;
+import Contact from './Contact';
+import data from './data';
 
 class Layout extends React.Component {
   render() {
     return(
       <div id='helloReact' className='pure-g'>
         { data.map(info =>
-          <Contact {...info}/>
+          <Contact key={info.id} {...info}/>
         )}
       </div>
     )
---
+++ src/components/data.js
@@ -0,0 +1,7 @@
+const data = [
+  { id: 1, name: 'Vlad', email: 'vlad@tut.by'},
+  { id: 2, name: 'Vlad2', email: 'vlad2@tut.by'},
+  { id: 3, name: 'Vlad3', email: 'vlad3@tut.by'}
+];
+
+export default data;
