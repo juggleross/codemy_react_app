459934c767f2b6dc592bb967b7459295f343594d
Vladislav Hilko <vladislav.spawn@gmail.com>
2018-09-27 22:32:01 +0300

    Chapter 9 - working with events

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,12 +4,20 @@ import Contact from './Contact';
 import data from './data';
 
 class Layout extends React.Component {
+  addContact = (e) => {
+    e.preventDefault();
+    alert('hello');
+  }
+
   render() {
     return(
-      <div id='helloReact' className='pure-g'>
-        { data.map(info =>
-          <Contact key={info.id} {...info}/>
-        )}
+      <div id='helloReact'>
+        <a href="#" className='pure-button' onClick={this.addContact}>Add me</a>
+        <div  className='pure-g'>
+          { data.map(info =>
+            <Contact key={info.id} {...info}/>
+          )}
+        </div>
       </div>
     )
   }
