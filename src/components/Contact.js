e9eced7daab4839edd47cd7376ee4b95d53e2ea3
Vladislav Hilko <vladislav_hilko@epam.com>
2018-10-01 12:03:44 +0300

    Chapter 15 - Add a link to the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import { Link } from 'react-router';
+
 const Contact = (props) =>
   <div className="pure-u-1-3">
-    <h1>{props.name}</h1>
+    <Link to={`contacts/${props.id}`}>
+      <h1>
+        {props.name}
+      </h1>
+    </Link>
+
     <p>{props.email}</p>
   </div>;
 
--- src/components/Show.js
+++ src/components/Show.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Contact from './Contact';
 
 class Show extends React.Component {
   render() {
     return (
       <div>
-        Hello
+        <h1>Test</h1>
+        <Contact></Contact>
       </div>
     )
   }
