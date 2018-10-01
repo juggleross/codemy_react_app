9749fd4a637e643ed99ad2649e38712e5704bf98
Vladislav Hilko <vladislav_hilko@epam.com>
2018-10-01 12:35:10 +0300

    Chapter 16 - show contact detail

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Contact from './Contact';
+import data from './data'
 
 class Show extends React.Component {
+  componentWillMount() {
+    this.setState({
+      contact: data.filter(elem => elem.id === parseInt(this.props.params.contactId))[0]
+    })
+  }
   render() {
     return (
       <div>
-        <h1>Test</h1>
-        <Contact></Contact>
+        <h1>{this.state.contact.name}</h1>
       </div>
     )
   }
