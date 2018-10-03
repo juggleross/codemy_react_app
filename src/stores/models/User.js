5fa50302868a6dc3ea956fd35c7d024099b722de
Vladislav Hilko <vladislav_hilko@epam.com>
2018-10-03 16:24:16 +0300

    Chapter 42 - Storing token in LocalStorage

--- src/stores/models/User.js
+++ src/stores/models/User.js
@@ -1,13 +1,35 @@
 import { observable, action } from 'mobx';
+import { browserHistory } from 'react-router';
 import Api from '../../lib/helpers/api/index';
 
 class User {
 
   @observable isLoading = false;
 
-  createSession(email, password) {
-    alert(email)
-    alert(password)
+  @action setIsLoading(status) {
+    this.isLoading = status;
+  }
+
+  async createSession(email, password) {
+    this.setIsLoading(true);
+
+    const response = await Api.post('/sessions', { email, password });
+
+    const status = await response.status;
+
+    if(status === 201) {
+      const response_with_data = await response.json();
+      const { user } = response_with_data.data;
+
+      localStorage.setItem('token', user.authentication_token);
+      localStorage.setItem('email', user.email);
+
+      this.setIsLoading(false);
+
+      browserHistory.push('/');
+    } else {
+      alert('unauthorize')
+    }
   }
 }
 
