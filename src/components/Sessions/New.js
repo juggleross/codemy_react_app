2565858814d0645474d08bedca1b0f67cd528861
Vladislav Hilko <vladislav_hilko@epam.com>
2018-10-03 15:21:16 +0300

    Chapter 40 - create fill in form

--- src/components/Sessions/New.js
+++ src/components/Sessions/New.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import styles from './New.module.sass';
 
 class New extends React.Component {
   render() {
     return (
-      <form action="">
-        This is a new action
-      </form>
+      <div className={styles.signInForm}>
+        <form action="submit" className='pure-form pure-form-stacked'>
+          <fieldset>
+            <label>Email</label>
+            <input type="email" placeholder='email' className='pure-input-1'/>
+            <label htmlFor="">Password</label>
+            <input type="password" className='pure-input-1'/>
+            <button className={`pure-button pure-input-1 ${styles.sigInButton}`}>Sign in</button>
+          </fieldset>
+        </form>
+      </div>
     )
   }
 }
